a84d934e05eca692e695b7d0339b14d3c635c896
Daksh Khetan <2012daksh@gmail.com>
2020-04-25 21:44:13 +0530

    destructure js object from API response

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,22 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+      
+    /* // const response = await axios.get(url);
+    const { data } = await axios.get(url);
+    const modifiedData = {
+      confirmed: data.confirmed,
+      recovered: data.recovered,
+      deaths: data.deaths,
+      lastUpdate: data.lastUpdate,
+    };
+    return modifiedData; */
+
+    // Cleaner code for above:
+    // destructuring javascript objects
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+    return { confirmed, recovered, deaths, lastUpdate };
+
   } catch(error) {
     // console.log(error);
   }
