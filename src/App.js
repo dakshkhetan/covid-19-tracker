b583ed33f9f625a12c992b39e781d510cd725d91
Daksh Khetan <2012daksh@gmail.com>
2020-04-26 19:03:36 +0530

    implement bar chart for country data

--- src/App.js
+++ src/App.js
@@ -20,18 +20,19 @@ export default class App extends Component {
 
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
-    console.log(fetchedData);
+    // console.log(fetchedData);
+    this.setState({ data: fetchedData, country: country });
   };
 
   render() {
 
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import {fetchDailyData} from '../../api';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
   const [dailyData, setDailyData] = useState({});
 
@@ -15,7 +15,7 @@ const Chart = () => {
       setDailyData(initialDailyData);
     };
     fetchMyAPI(); // to use asynce-await functionality inside useEffect()
-  });
+  }, []);
 
   const lineChart = (
     dailyData[0] ? (
@@ -40,9 +40,30 @@ const Chart = () => {
     ) : null
   );
 
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
+
   return (
     <div className={styles.container}>
-      { lineChart }
+      {country ? barChart : lineChart}
     </div>
   )
 }
